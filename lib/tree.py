815f4befce044640ee6ee045c9457f09f9864326
zakariamohamed123 <zakimoha20222@gmail.com>
2024-03-26 18:34:49 +0300

    final

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,12 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    node = self.root
+    nodes_to_visit = [node]
+
+    while len(nodes_to_visit) > 0:
+      if node['id'] == id:
+        return node
+      node = nodes_to_visit.pop(0)
+      nodes_to_visit = node['children'] + nodes_to_visit
+    return None
